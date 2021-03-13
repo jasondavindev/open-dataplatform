450e88f9a95a542a931c70125a6ec3b5b08d33ca
Jason Carneiro <jasondavin.dev@gmail.com>
2021-03-12 23:23:01 -0300

    feat: add trino; fix: spark hive support (#8)

--- spark/scripts/hive_integration.py
+++ spark/scripts/hive_integration.py
@@ -1,33 +1,34 @@
 from pyspark.sql import SparkSession
 
+database = 'spark_test'
+table = 'users'
+
 spark = SparkSession \
     .builder \
     .appName("Python Spark SQL Hive integration example") \
     .enableHiveSupport() \
     .getOrCreate()
 
-hdfs_uri = 'hdfs://namenode:8020'
-database = 'spark_warehouse'
-hive_warehouse = '/user/hive/warehouse/'
-database_location = f"{hdfs_uri}{hive_warehouse}{database}"
-table = 'users'
-
 spark.sql(f"CREATE DATABASE IF NOT EXISTS {database}")
 
-columns = ['name', 'country', 'dt']
+columns = ['id', 'name', 'dt']
 values = [
-    ('jason', 'brazil', '2021-03-12'),
-    ('juca', 'italy', '2021-03-12'),
-    ('juju', 'brazil', '2021-03-11'),
-    ('jubileu', 'germany', '2021-03-10')
+    (1, 'jason', '2021-03-12'),
+    (2, 'davin', '2021-03-11'),
 ]
 
 df = spark.createDataFrame(values, columns)
 
 df.show()
 
-df.repartition('dt') \
+df \
+    .repartition('dt') \
     .write \
     .mode('overwrite') \
     .partitionBy(['dt']) \
-    .saveAsTable(f"{database}.{table}", path=database_location)
+    .saveAsTable(
+        f"{database}.{table}",
+        path=f"hdfs://namenode:8020/user/hive/warehouse/{database}/{table}",
+        overwrite=True)
+
+spark.stop()
