49815faf85bd5fd45ada21a56665dfcb8d27633c
jasondavindev <jasondavin.dev@gmail.com>
2021-04-21 15:30:38 -0300

    chore: change hadoop image

--- hdfs/entrypoint.sh
+++ hdfs/entrypoint.sh
@@ -1,46 +1,6 @@
 #!/bin/bash
-DK_HOSTNAME=$(hostname -A)
-export CORE_DEFAULT_FS_VALUE=${HDFS_NAMENODE_URL:-`printf "hdfs://%s:8020" $DK_HOSTNAME`}
+if [ "$IS_MASTER" == "1" -a -z "$(ls /hadoop/hdfs/data)" ];then
+  hdfs namenode -format
+fi
 
-echo "Replacing core-site defaultFS=$CORE_DEFAULT_FS_VALUE"
-
-sed -i "s~CORE_DEFAULT_FS_VALUE~${CORE_DEFAULT_FS_VALUE}~g" $HADOOP_CONF_DIR/core-site.xml
-
-node_type=$1
-
-function check_directory() {
-    local path=$1
-    local node_type=$2
-
-    namedir=${path#"file://"}
-    if [ ! -d $namedir ]; then
-        echo "Creating not existing $node_type directory: $namedir"
-        mkdir -p $namedir
-    fi
-}
-
-case "$node_type" in
-    (namenode)
-        shift
-        namedir=${HDFS_NAMENODE_NAME_DIR#"file://"}
-        check_directory $namedir namenode
-
-        if [ "`ls -A $namedir`" == "" ]; then
-          echo "Formatting namenode name directory: $namedir"
-          $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode -format $CLUSTER_NAME
-        fi
-
-        $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode $@
-      ;;
-
-    (datanode)
-      shift
-        namedir=${HDFS_DATANODE_DATA_DIR#"file://"}
-        check_directory $namedir datanode
-
-        $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR datanode $@
-      ;;
-    (*)
-      exec "$@"
-      ;;
-esac
+exec "$@"
