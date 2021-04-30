6a3d13f0ac8ba054cb04ca8b5cd9edfa2be33f52
Jason Carneiro <jasondavin.dev@gmail.com>
2021-04-30 08:42:01 -0300

    chore: add single function to call status invest; add fiis research (#27)

--- airflow/plugins/dataplatform/operators/status_invest/status_invest_to_hdfs_operator.py
+++ airflow/plugins/dataplatform/operators/status_invest/status_invest_to_hdfs_operator.py
@@ -5,24 +5,33 @@ from dataplatform.hooks.status_invest_hook import StatusInvestHook
 from dataplatform.hooks.hdfs_hook import HDFSHook
 
 
-class StocksToHDFSOperator(BaseOperator):
+class StatusInvestToHDFSOperator(BaseOperator):
     def __init__(
             self,
             dt=None,
             status_invest_conn_id='status_invest_conn',
             hdfs_conn_id='hdfs_http',
+            category_type='stocks',
             *args,
             **kwargs):
         self.status_invest_client = StatusInvestHook(
             conn_id=status_invest_conn_id)
         self.hdfs_hook = HDFSHook(hdfs_conn_id, 'airflow')
         self.dt = dt
+        self.category_type = category_type
+        self.define_map_category_type()
 
         super().__init__(*args, **kwargs)
 
+    def define_map_category_type(self):
+        self.category_types = {
+            'stocks': self.status_invest_client.get_all_stocks,
+            'fiis': self.status_invest_client.get_all_fiis
+        }
+
     def execute(self, context):
-        stocks = self.status_invest_client.get_all_stocks()
+        data = self.category_types[self.category_type]()
         return self.hdfs_hook.put(
-            path=f"/airflow/status_invest/dt={self.dt}/result.json",
-            data=json.dumps(stocks),
+            path=f"/airflow/staging/status_invest/{self.category_type}/dt={self.dt}/result.json",
+            data=json.dumps(data),
             overwrite=True)
