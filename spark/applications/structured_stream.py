0b3670a58c475b714d05ba95060c52f1cb1f4aea
jasondavindev <jasondavin.dev@gmail.com>
2021-05-20 22:36:04 -0300

    chore: send messages to kafka

--- spark/applications/structured_stream.py
+++ spark/applications/structured_stream.py
@@ -1,9 +1,9 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.avro.functions import from_avro
-from pyspark.sql.functions import count, expr, col, to_json, window, avg
+from pyspark.sql.functions import count, expr, col, struct, to_json, window, avg
 from pyspark.sql.types import StringType
 
-APPLICATION_TOPIC = 'example-2'
+APPLICATION_TOPIC = 'example'
 
 jsonFormatSchema = """
 {
@@ -36,22 +36,16 @@ output = df \
     .withColumn('parsedValue', from_avro('fixedValue', jsonFormatSchema,  {"mode": "FAILFAST"})) \
     .select(col('parsedValue.event_time').cast('timestamp').alias('value'), 'parsedValue.name', 'parsedValue.favorite_color') \
     .withWatermark('value', '5 seconds') \
-    .groupBy(window('value', '5 seconds', '5 seconds'), 'favorite_color') \
-    .agg(count('*').alias('count'))
+    .groupBy(window('value', '5 seconds', '5 seconds').alias('window'), 'favorite_color') \
+    .agg(count('favorite_color').alias('count')) \
+    .select(to_json(struct(['window', 'favorite_color', 'count'])).alias("value"))
 
-query = output \
-    .writeStream \
-    .outputMode('complete') \
-    .option('truncate', False) \
-    .format('console') \
+query = output\
+    .writeStream\
+    .format("kafka")\
+    .option("kafka.bootstrap.servers", "broker:29092")\
+    .option("topic", "output-topic")\
+    .option("checkpointLocation", "/tmp/checkpoint")\
     .start()
-
-# query = output\
-#     .writeStream\
-#     .format("kafka")\
-#     .option("kafka.bootstrap.servers", "broker:29092")\
-#     .option("topic", "output-topic")\
-#     .option("checkpointLocation", "/tmp/checkpoint")\
-#     .start()
 
 query.awaitTermination()
