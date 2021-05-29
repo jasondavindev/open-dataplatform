0e3362c98ad242eba9db576fd5ffd2024e249789
Jason Carneiro <jasondavin.dev@gmail.com>
2021-05-28 22:55:24 -0300

    feat: change schema and fix clickstream script (#41)

--- ingestion/scripts/spam_api.sh
+++ ingestion/scripts/spam_api.sh
@@ -5,18 +5,26 @@ DELAY=$2
 [ -z "$TOPIC" -o -z "$DELAY" ] && echo "Usage script.sh topic-name delay" && exit 1
 
 USERNAMES=('John' 'Mary' 'Cris')
-COLORS=('black' 'red' 'blue')
+PAGES=('home' 'users' 'login')
+EVENT_NAMES=('click' 'scroll' 'submit')
 
 PAYLOAD='{
-    "name": "USERNAME",
-    "favorite_color": "COLOR"
+    "username": "USERNAME",
+    "page": "PAGE",
+    "event_name": "EVENT_NAME",
+    "event_time": EVENT_TIME
 }'
 
 while true; do
     RANDOM_USER=$((RANDOM % 3)) # between 0 and 2
-    RANDOM_COLOR=$((RANDOM % 3))
+    RANDOM_PAGE=$((RANDOM % 3))
+    RANDOM_EVENT=$((RANDOM % 3))
 
-    REQUEST_PAYLOAD=$(echo $PAYLOAD | sed -e "s|USERNAME|${USERNAMES[$RANDOM_USER]}|g" -e "s|COLOR|${COLORS[$RANDOM_COLOR]}|g")
+    REQUEST_PAYLOAD=$(echo $PAYLOAD | sed \
+        -e "s|USERNAME|${USERNAMES[$RANDOM_USER]}|g" \
+        -e "s|PAGE|${PAGES[$RANDOM_PAGE]}|g" \
+        -e "s|EVENT_NAME|${EVENT_NAMES[$RANDOM_EVENT]}|g" \
+        -e "s|EVENT_TIME|$(date +%s)|g")
 
     curl -X POST \
         -d "$REQUEST_PAYLOAD" \
