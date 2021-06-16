006d207e4179aab3a89bc8b143bc408f6115044e
jasondavindev <jasondavin.dev@gmail.com>
2021-06-16 19:32:24 -0300

    feat: separate of concern - add kafka wrapper to new module

--- ingestion/index.js
+++ ingestion/index.js
@@ -1,32 +1,21 @@
 const dotenv = require('dotenv');
 const express = require('express');
-const { createRegistry } = require('./lib/schema_registry');
-const { createProducer } = require('./lib/producer');
+const { StreamingModule } = require('./lib/kafka_wrapper/wrapper');
 
 dotenv.config();
 
 (async () => {
   const { PORT, SCHEMA_REGISTRY_URL, KAFKA_BROKER } = process.env;
-  const producer = await createProducer(KAFKA_BROKER);
-  const registry = createRegistry(SCHEMA_REGISTRY_URL);
 
-  const app = express();
-  app.use(express.json());
-
-  app.get('/status', (_, res) => res.json({ status: 'ok' }));
-
-  app.post('/event/:topic', async (req, res) => {
-    const body = req.body;
-    const topic = req.params.topic;
-    const response = { topic, body };
-    const payload = await registry.encodeMessage(topic, body);
-
-    console.log(`Sending message payload=${JSON.stringify(body)}`);
-
-    await producer.sendMessage(topic, payload);
-
-    return res.status(201).json(response);
+  const streamingModule = await StreamingModule.factory({
+    SCHEMA_REGISTRY_URL,
+    KAFKA_BROKER,
   });
 
+  const app = express();
+
+  app.use(express.json());
+  app.get('/status', (_, res) => res.json({ status: 'ok' }));
+  app.post('/event/:topic', streamingModule.sendMessage);
   app.listen(PORT, () => console.log('Server listening on port', PORT));
 })();
