fb19cab2bb5c87ad1bd35d7a6256b76bb9ad6d34
jasondavindev <jasondavin.dev@gmail.com>
2021-09-23 17:45:07 -0300

    add kafka setup

--- ingestion/index.js
+++ ingestion/index.js
@@ -6,20 +6,25 @@ const { ContentGenerator } = require('./lib/generator/index');
 dotenv.config();
 
 (async () => {
-  const { PORT, SCHEMA_REGISTRY_URL, KAFKA_BROKER } = process.env;
+  const { PORT, SCHEMA_REGISTRY_URL, KAFKA_BROKER, HAS_KAFKA } = process.env;
 
-  const streamingModule = await StreamingModule.factory({
-    SCHEMA_REGISTRY_URL,
-    KAFKA_BROKER,
-  });
   const contentGenerator = ContentGenerator.factory();
 
   const app = express();
 
   app.use(express.json());
   app.get('/status', (_, res) => res.json({ status: 'ok' }));
-  app.post('/event/:topic', streamingModule.sendMessage);
   app.get('/records', contentGenerator.generateRecords);
   app.get('/record', contentGenerator.generateRecord);
+
+  if (HAS_KAFKA) {
+    const streamingModule = await StreamingModule.factory({
+      SCHEMA_REGISTRY_URL,
+      KAFKA_BROKER,
+    });
+
+    app.post('/event/:topic', streamingModule.sendMessage);
+  }
+
   app.listen(PORT, () => console.log('Server listening on port', PORT));
 })();
