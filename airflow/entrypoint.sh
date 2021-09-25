dd38373eb79e5fd725f30649b9e7ae1ec0d6bd51
jasondavindev <jasondavin.dev@gmail.com>
2021-09-24 21:44:08 -0300

    Fix setup

--- airflow/entrypoint.sh
+++ airflow/entrypoint.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-for file in $AIRFLOW_HOME/bootstrap/*.sh; do
-  bash "$file" -H
-done
+function bootstrap {
+  for file in $AIRFLOW_HOME/bootstrap/*.sh; do
+    bash "$file" -H
+  done
+}
 
-exec "${@}"
+case "$1" in
+  webserver)
+    bootstrap
+    sleep 2
+    exec airflow "$@"
+    ;;
+  scheduler)
+    exec airflow "$@"
+    ;;
+  *)
+    exec "$@"
+    ;;
+esac
