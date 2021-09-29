84a332c00ba5cf6703eb0f34d6c37fe055d5d875
Jason Carneiro <jasondavin.dev@gmail.com>
2021-09-29 20:11:16 -0300

    Fix Airflow Spark setup (#70)
    * Changing airflow submit operator
    
    * remote plugin folder from sync
    
    * remove unnecessary print
    
    * change sync branch

--- airflow/plugins/dataplatform/operators/spark/docker_spark_submit_operator.py
+++ airflow/plugins/dataplatform/operators/spark/docker_spark_submit_operator.py
@@ -1,4 +1,5 @@
 import ntpath
+import os
 from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 from airflow.hooks.webhdfs_hook import WebHDFSHook
 from airflow.hooks.base_hook import BaseHook
@@ -8,8 +9,11 @@ class DockerSparkSubmitOperator(SparkSubmitOperator):
     def __init__(self,
                  hdfs_http_conn_id='hdfs_http',
                  hdfs_conn_id='hdfs',
+                 conn_id='spark',
                  *args,
                  **kwargs):
+        kwargs['conn_id'] = 'spark-cluster' if os.getenv(
+            'SPARK_DEPLOY_MODE') == 'cluster' else conn_id
         super().__init__(*args, **kwargs)
         self._hdfs_hook = WebHDFSHook(hdfs_http_conn_id)
         self._hdfs_http_conn_id = hdfs_http_conn_id
@@ -31,9 +35,30 @@ class DockerSparkSubmitOperator(SparkSubmitOperator):
         return f"{self._hdfs_conn_string}/{self._new_application_path}"
 
     def load_script(self):
+        path_prefix = os.getenv("SCRIPTS_PATH_PREFIX") or ""
+        print(f"Uploading file {path_prefix}{self._old_application}")
+
         self._hdfs_hook.load_file(
-            self._old_application, self._new_application_path, overwrite=True)
+            f"{path_prefix}{self._old_application}",
+            self._new_application_path,
+            overwrite=True)
+
+    def define_default_confs(self):
+        if os.getenv('SPARK_DEPLOY_MODE') != 'cluster':
+            return self._conf
+
+        self._conf = {
+            **(self._conf or {}),
+            **{
+                'spark.kubernetes.container.image': 'open-dataplatform-spark:3.1.2',
+                'spark.kubernetes.authenticate.driver.serviceAccountName': 'spark',
+                'spark.kubernetes.namespace': 'dataplatform',
+                'spark.hadoop.hive.metastore.uris': 'thrift://hive-metastore-svc.dataplatform.svc.cluster.local:9083'
+            }}
+
+        return self._conf
 
     def execute(self, context):
+        self.define_default_confs()
         self.load_script()
         super().execute(context)
